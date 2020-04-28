47e9f06b11bbde689ff086a53e24e6d7607462d1
TheFLAMEffect <theflameffect@gmail.com>
2020-04-27 21:30:15 -0500

    99% done with logic prototype

--- P4Server/src/main/java/ServerLogic.java
+++ P4Server/src/main/java/ServerLogic.java
@@ -115,7 +115,7 @@ public class ServerLogic {
         }
 
         // logic for the game
-        public void guessingGameLogic() {
+        public void lossCheck(ArrayList<String> words, ArrayList<String> categories) {
 
         }
 
@@ -129,28 +129,36 @@ public class ServerLogic {
             }// end of try catch
 
             GuessInfo serverInfo = new GuessInfo();
+            GuessInfo playerInfo;
+
+            boolean guess = true;
+            boolean category = false;
+            boolean gameOver = false;
+
+            Random rand = new Random();
+
+            int randomNum =  rand.nextInt(10);
+            ArrayList<String> words = new ArrayList<String>();
+            String wordCreation = "";
+            char letter;
 
             try {
-                GuessInfo playerInfo = (GuessInfo) in.readObject();
+                playerInfo = (GuessInfo) in.readObject();
                 serverInfo.setCategories(playerInfo.getCategories().get(0)); // disgosten line.
             }
             catch (Exception e){
                 System.out.println("Ain't workin' properly");
             }
 
-            Random rand = new Random();
-
-            int randomNum =  rand.nextInt(10);
-            String word;
 
             if(serverInfo.getCategories().get(0) == "Games")
-                word = gamesCategoryWords.get(randomNum);
+                words.add(gamesCategoryWords.get(randomNum));
             else if(serverInfo.getCategories().get(0) == "Sports")
-                word = sportsCategoryWords.get(randomNum);
+                words.add(sportsCategoryWords.get(randomNum));
             else // If the wrong word given is from category Foods then this might be the issue.
-                word = foodCategoryWords.get(randomNum);
+                words.add(foodCategoryWords.get(randomNum));
 
-            serverInfo.setWord(word.length());
+            serverInfo.setWord(words.get(0).length());
             try {
                 out.writeObject(serverInfo);
             }
@@ -160,11 +168,133 @@ public class ServerLogic {
 
             while(true) {
                 try {
+                    // In the guessing phase
+                    if(guess) {
 
+                        playerInfo = (GuessInfo) in.readObject(); // Waits for the player's response here
 
+                        letter = playerInfo.getGuesses().get(playerInfo.getGuesses().size() - 1); // Store the response to the variable letter
+                        // Check if the letter is unique from all the guesses the player made
+                        if(playerInfo.getGuesses().indexOf(letter) != playerInfo.getGuesses().size() - 1){
+                            callback.accept("Player " + number + " guessed a letter twice");
+                            continue; // If not then try again.
+                        }
+                        else {
+                            serverInfo.setGuesses(letter);
+                            // If the letter doesn't exist in the word
+                            if(words.get(words.size() - 1).indexOf(letter) == -1){ // Still needs so much work
+                                serverInfo.setNumWrongGuesses(serverInfo.getNumWrongGuesses() + 1);
+                                if(serverInfo.getNumWrongGuesses() == 7){
+                                    serverInfo.setNumWordsGuessed(serverInfo.getNumWordsGuessed() + 1);
+                                    serverInfo.clearGuesses();
+                                    serverInfo.removeCategory();
+
+                                    callback.accept("Player " + number + " lost the guess round!");
+                                    guess = false;
+                                    category = true;
+                                    gameOver = false;
+
+                                    int gameCount = 0;
+                                    int foodCount = 0;
+                                    int sportCount = 0;
+                                    int size = words.size(); // just to make it look better
+
+                                    // this for loops just counts up the words based on category (hopefully none are >= 4)
+                                    for(int i = 0; i < size; i++){
+//                                        if(serverInfo.getCategories().get(i).equals(serverInfo.getCategories().get(size - 1)))
+//                                            count++;
+                                        if(gamesCategoryWords.contains(words.get(i)))
+                                            gameCount++;
+                                        else if (foodCategoryWords.contains(words.get(i)))
+                                            foodCount++;
+                                        else if(sportsCategoryWords.contains(words.get(i)))
+                                            sportCount++;
+                                    }
+                                    if((gameCount == 3 && gamesCategoryWords.contains(words.get(size - 1)))
+                                    || (foodCount == 3 && foodCategoryWords.contains(words.get(size - 1)))
+                                    || (sportCount == 3 && sportsCategoryWords.contains(words.get(size - 1)))){
+                                        callback.accept("Player " + number + " lost the game!");
+                                        guess = false;
+                                        category = false;
+                                        gameOver = true;
+                                    }
+//                                    if(count == 3){ // If the player messes up the guess on the same category 3 times
+//                                    callback.accept("Player " + number + " lost the game!");
+//                                    guess = false;
+//                                    category = false;
+//                                    gameOver = true;
+//                                    }
+
+                                }
+                                out.writeObject(serverInfo);
+                            }
+                            // If the letter exists in the word being guessed
+                            else{
+                                for(int i = 0; i < words.get(words.size() - 1).length(); i++){
+                                    if(words.get(words.size() - 1).charAt(i) == letter)
+                                        wordCreation.concat(Character.toString(letter));
+                                    else
+                                        wordCreation.concat("_");
+                                }
+                                serverInfo.setWord(wordCreation);
+                                // If the player guesses the entire word correctly.
+                                if(wordCreation.equals(words.get(words.size() - 1))){
+                                    callback.accept("Player " + number + " guessed the word correctly!");
+                                    serverInfo.setNumWordsGuessed(serverInfo.getNumWordsGuessed() + 1);
+                                    serverInfo.clearGuesses();
+                                    guess = false;
+                                    category = true;
+                                    gameOver = false;
+
+                                }
+                                out.writeObject(serverInfo); // Send the GuessInfo after the guess.
+                            }
+                        }
+                    }
+                    else if(category){
+                        playerInfo = (GuessInfo) in.readObject();
+                        serverInfo.setCategories(playerInfo.getCategories().get(playerInfo.getCategories().size() - 1));
+
+                        while(true) {
+                            randomNum = rand.nextInt(10);
+
+                            int lastIndex = serverInfo.getCategories().size() - 1;
+
+                            String randWord;
+                            if (serverInfo.getCategories().get(lastIndex) == "Games")
+                                //words.add(gamesCategoryWords.get(randomNum));
+                                randWord = gamesCategoryWords.get(randomNum);
+                            else if (serverInfo.getCategories().get(lastIndex) == "Sports")
+                                //words.add(sportsCategoryWords.get(randomNum));
+                                randWord = sportsCategoryWords.get(randomNum);
+                            else // If the wrong word given is from category Foods then this might be the issue.
+                                //words.add(foodCategoryWords.get(randomNum));
+                                randWord = foodCategoryWords.get(randomNum);
+
+                            if(!words.contains(randWord)){
+                                words.add(randWord);
+                                break;
+                            }
+                        }
+
+                        serverInfo.clearGuesses();
+                        serverInfo.setNumWrongGuesses(0);
+                        serverInfo.setWord(words.get(words.size() - 1).length());
+                        out.writeObject(serverInfo);
+
+                        guess = true;
+                        category = false;
+                        gameOver = false;
+
+                    }
+                    else if(gameOver){
+                        playerInfo = (GuessInfo) in.readObject();
+
+                    }
                 }
                 catch (Exception e) {
-                    callback.accept("Something is wrong with the socket connection");
+                    callback.accept("Something is wrong with the socket connection for player " + number);
+                    clients.remove(this);
                     break;
                 }
 //                run game functionality
